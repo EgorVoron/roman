071c1f214429ae6a2d6fcf4b2fd29dad758fd59c
EgorVoron <ekvoron01@gmail.com>
2020-03-26 14:44:10 +0300

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+const name = 'ухоботов'.split('');
+
+let shuffledName = shuffle(name);
+
+shuffledName[0] = shuffledName[0].toUpperCase()
+
+const data = {
+  message: (shuffledName),
+}
+
+$('#msg').html(data.message)
